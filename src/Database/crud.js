4b82d646ac6d60d3b59391649751450b4c7d1591
DarkLazoril <darkgaming-lazoril@outlook.fr>
2023-03-23 17:18:21 +0000

    add create

--- src/Database/crud.js
+++ src/Database/crud.js
@@ -1,5 +1,6 @@
 import db from './connection.js'
 import Wallpaper from './model.js'
+import bg1 from './'
 
 let newWallpapers = [
   new Wallpaper({
@@ -19,5 +20,18 @@ for (const wallpaper of newWallpapers) {
     console.log('ADDED')
   } catch (error) {
     console.log('ERROR: ' + error)
+  }
+}
+async function create(req, res) {
+  try {
+    const newWallpaper = await Wallpaper.create({
+      title: req.body.title,
+      image: req.file.buffer,
+      created_at: Date.now(),
+    })
+    await newWallpaper.save()
+    res.status(201).json(newWallpaper)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
   }
 }
